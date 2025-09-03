46a66677bfde714f22df1dd1ddb34b8935d7e654
Santiago <santiyluci2017@gmail.com>
2025-09-03 18:46:58 -0500

    Santiago Gonzalez PROGRAMADOR B AGREGADO METODO A

--- src/main/java/com/pracitcaparcial/parcial/Inventario.java
+++ src/main/java/com/pracitcaparcial/parcial/Inventario.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.pracitcaparcial.parcial;
 
-/**
- *
- * @author BryanVanegas
- */
 public class Inventario {
-    
+
+    private void metodoA() {
+        System.out.println("EJECUTANDO METODO A");
+    }
+
 }
